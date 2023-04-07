fed32b02cc9e672320c868b60e8dcac2ad644a0c
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-04-07 09:33:00 -0400

    Update Initial_MQTT.py

--- Proof of Concept - MQTT/Initial_MQTT.py
+++ Proof of Concept - MQTT/Initial_MQTT.py
@@ -2,12 +2,31 @@ import paho.mqtt.client as mqtt
 
 mqtt_broker = "your_MQTT_BROKER_IP_ADDRESS"
 
+# add an empty string vector
+
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT Broker with result code "+str(rc))
+    #init_plot       inilize plot
     client.subscribe("esp32/#")
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    #array.append(strToInt(msg.payload)        call function to turn string into data value. 
+    #storeInFile(strToInt(msg.payload))
+    #updatePlot(array name)
+    
+    
+# def function to turn the msg into a int value
+    # return a int
+
+    
+# def print value (value array)
+    # stuff to print plot
+    
+#def init_plot
+    #inlilie plot with features 
+
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
