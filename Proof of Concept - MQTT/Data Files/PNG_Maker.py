a9044ade167a9d71f6f0173bf84aaa4f592ae633
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-04-21 15:07:42 -0400

    Final Commit

--- Proof of Concept - MQTT/Data Files/PNG_Maker.py
+++ Proof of Concept - MQTT/Data Files/PNG_Maker.py
@@ -1,20 +1,39 @@
 # This file will read a .txt file, plot the values of the data and save an svg of the plot 
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+
+tempatureList = []  # Create an empty list
+timeList = []  # Create an empty list
+
+
 
 # Open the text file for reading
-with open('/Proof of Concept - MQTT/dataFile1.txt', 'r') as file:
+#/Proof of Concept - MQTT/
+with open('static/dataFile1.txt', 'r') as file:
     # Read the last 20 lines of the file
     lines = file.readlines()[-20:]
 
-# Convert each line to a float and store in a list
-data = [float(line.strip()) for line in lines]
 
-# Plot the data using Matplotlib
-plt.plot(data)
-plt.ylabel('Tempature (Degrees C)')
-plt.title('Tempature of Lab')
-plt.savefig('plot.png')
+for i in range(20):
+    parts = lines[i].split(" ")
+    tempatureList.append(float(parts[0]))
+    timeList.append(parts[1])
+    
+# # Plot the data
+plt.figure(figsize=(13,3))
+plt.plot(timeList, tempatureList)
 
+# Add labels and title
+plt.xlabel("Tempature (Degrees C)")
+plt.ylabel("Time (Hr : Min)")
+plt.title("Tempature of Lab")
 
+# Show the plot
+plt.savefig("static/plot1.png")
 plt.show()
+
+
+
+
 
